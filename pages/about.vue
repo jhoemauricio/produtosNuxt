0b320f5e42eff62c8beacfeb3aaf679b62657dc1
jhoemauricio <jhosephmauriciojhoseph@gmail.com>
2023-09-25 17:42:26 -0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p> Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão.
+            Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor
+            desconhecido pegou uma galera de tipos e os embaralhou para fazer um livro de espécimes de tipos.
+        </p>
+        <p> Lorem Ipsum é simplesmente um texto fictício da indústria tipográfica e de impressão.
+            Lorem Ipsum tem sido o texto fictício padrão da indústria desde os anos 1500, quando um impressor
+            desconhecido pegou uma galera de tipos e os embaralhou para fazer um livro de espécimes de tipos.
+        </p>
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+    h2 {
+
+        margin-bottom: 20px;
+        font-size: 36px;
+        
+    }
+    p {
+
+        margin: 20px 0;
+
+    }
+</style>
